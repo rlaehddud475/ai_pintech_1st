f3fd6b22fd007cfdbeba42d3704d3bd1296a59dd
rlaehddud475 <6070kjh475@naver.com>
2024-10-07 16:49:03 +0900

    20241007

---
+++ Day01/day01/src/exam01/Ex10.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Ex10 {
+    public static void main(String[] args) {
+        int num1= 1000000000;
+        float num2=num1;
+
+        long num3=100000000000l;
+        float num4=num3;
+    }
+}
---
+++ Day01/day01/src/exam01/Ex11.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex11 {
+    public static void main(String[] args) {
+        int num1=10;
+        double num2=3.0;
+
+
+    double result =num1/num2;
+        System.out.println(result);
+    }
+}
