09f91550f5f45a4ac7c70c8dec8cbd3e36be202e
rlaehddud475 <6070kjh475@naver.com>
2024-10-07 16:00:30 +0900

    20241007

---
+++ Day01/day01/src/exam01/Ex07.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex07 {
+    public static void main(String[] args) {
+        int num1 = 1000000000;
+        byte num2=100;
+        long num3=10_000_000_000l;
+    }
+}
--- Day01/day01/src/exam01/Ex09.java
+++ Day01/day01/src/exam01/Ex09.java
@@ -1,8 +1,7 @@
 package exam01;
 
-public class Ex06 {
+public class Ex09 {
     public static void main(String[] args) {
-        char ch1='A';
-        char ch2='A';
+        
     }
 }
