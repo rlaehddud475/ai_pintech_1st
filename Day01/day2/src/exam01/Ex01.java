49eb5d331504c0c5636f0efaf23af21f824c1373
rlaehddud475 <6070kjh475@naver.com>
2024-10-08 10:40:04 +0900

    20241008

---
+++ Day01/day2/src/exam01/Ex01.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num1=10;
+       /* int num2=num1*-1;*/
+        int num2= -num1;
+        System.out.println(num2);
+        int num3=-num2;
+        System.out.println(num3);
+    }
+}
