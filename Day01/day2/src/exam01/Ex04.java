94acc303ea82253a0db32c2a635493e744339df0
rlaehddud475 <6070kjh475@naver.com>
2024-10-08 10:55:22 +0900

    20241008

--- Day01/day2/src/exam01/Ex04.java
+++ Day01/day2/src/exam01/Ex04.java
@@ -1,4 +1,11 @@
 package exam01;
 
 public class Ex04 {
+    public static void main(String[] args) {
+        int num1=10;
+        //int num2=num1++; 대입(=) -> 증감연산(++)
+        int num2=++num1; // 증감연선(++)->대입(=)
+        System.out.println(num2);
+        System.out.println(num1);
+    }
 }
