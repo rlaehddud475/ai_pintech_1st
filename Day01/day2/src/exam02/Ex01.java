257e2f639b5b6621b5329e6c05174d59423bed3d
rlaehddud475 <6070kjh475@naver.com>
2024-10-08 14:45:56 +0900

    20241008

--- Day01/day2/src/exam02/Ex01.java
+++ Day01/day2/src/exam02/Ex01.java
@@ -1,4 +1,12 @@
 package exam02;
 
 public class Ex01 {
+    public static void main(String[] args) {
+        int num = 15;
+        if (num==10){//조건식이 참일때 실행함
+            System.out.println("10 입니다.");
+        }else {
+            System.out.println("10이 아니무니다.");
+        }
+    }
 }
