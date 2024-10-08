4b014f8e4e465e86b620b6e2d43ba79ec6ca77fc
rlaehddud475 <6070kjh475@naver.com>
2024-10-08 11:54:31 +0900

    20241008

--- Day01/day2/src/exam01/Ex06.java
+++ Day01/day2/src/exam01/Ex06.java
@@ -2,9 +2,18 @@ package exam01;
 
 public class Ex06 {
     public static void main(String[] args) {
-        boolean reseult1 = 10>5;
-        System.out.println(reseult1);
+        boolean result1 = 10>5;
+//        System.out.println(result1);
         int num1 = 50;
-        
+        boolean result2=num1>10;
+        boolean result3=num1<100;
+//        System.out.println(result2);
+//        System.out.println(result3);
+
+        boolean result4=result2&& result3;
+        boolean result5= num1>=10 && num1<100;
+        System.out.println(result4);
+        System.out.println(result5);
+
     }
 }
