237354124cd928a0eaa24ded9169d98cda5e56f2
rlaehddud475 <6070kjh475@naver.com>
2024-10-08 16:27:06 +0900

    20241008

--- Day01/day2/src/exam03/Ex04.java
+++ Day01/day2/src/exam03/Ex04.java
@@ -2,6 +2,22 @@ package exam03;
 
 public class Ex04 {
     public static void main(String[] args) {
+    int total = 0;
+    for (int i = 1; i<=100; i++){
+//        if (i==50){
+//            continue;//건너뛰기
+//        }
+        if (i%2==0){
+continue;
+        }
+        total+=i;
+
+        /*
+        if(i==50){
+            break;//반복중단하는 키워드
+        }*/
+    }
+        System.out.println(total);
 
     }
 }
---
+++ Day01/day2/src/exam03/Ex05.java
@@ -0,0 +1,7 @@
+package exam03;
+
+public class Ex05 {
+    public static void main(String[] args) {
+
+    }
+}
