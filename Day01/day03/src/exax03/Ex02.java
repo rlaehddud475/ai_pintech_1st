0e538c173aae636a6a7df20be78bd72e00208a9e
rlaehddud475 <6070kjh475@naver.com>
2024-10-10 17:13:43 +0900

    20241010

--- Day01/day03/src/exax03/Ex02.java
+++ Day01/day03/src/exax03/Ex02.java
@@ -2,5 +2,10 @@ package exax03;
 
 public class Ex02 {
     public static void main(String[] args) {
-    }
+        System.out.println(calc(20));
+}
+static int calc(int x){
+        int y= x*2+3;
+        return y;
+}
 }
