acbdff2c914d39fc2621e566b4e825be1db47c20
rlaehddud475 <6070kjh475@naver.com>
2024-10-10 10:45:42 +0900

    20241010

--- Day01/day03/src/exam01/Ex01.java
+++ Day01/day03/src/exam01/Ex01.java
@@ -3,10 +3,12 @@ package exam01;
 public class Ex01 {
     public static void main(String[] args) {
 for (int i = 2; i<10; i++){
-    System.out.println("--- "+i+"단 ---");
+    System.out.printf("--- %d단 ---%n", i);
     for (int j=1; j<10; j++){
-        System.out.println(i+"X"+j+"="+(i*j));
+        System.out.printf("%d X %d = %d%n",i,j,(i*j));
     }
 }
+System.out.println( "가");
+        System.out.print("나");
     }
 }
---
+++ Day01/day03/src/exam01/Q1.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Q1 {
+    public static void main(String[] args) {
+        int num1=20;
+        double num2=3.63;
+          /*      int total=num1*(int)num2;*/
+    int total = (int)(num1*num2);
+        System.out.println(total);
+    }
+}
