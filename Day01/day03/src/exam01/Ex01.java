869f3fd81eb6b32e0e0d5acc21770e0198ef81fa
rlaehddud475 <6070kjh475@naver.com>
2024-10-10 09:52:03 +0900

    20241010

--- Day01/day03/src/exam01/Ex01.java
+++ Day01/day03/src/exam01/Ex01.java
@@ -2,6 +2,11 @@ package exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
-        
+for (int i = 2; i<10; i++){
+    System.out.println("--- "+i+"단 ---");
+    for (int j=1; j<10; j++){
+        System.out.println(i+"X"+j+"="+(i*j));
+    }
+}
     }
 }
