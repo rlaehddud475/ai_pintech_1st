4294c75812e038b67517c700a50e175642b89800
rlaehddud475 <6070kjh475@naver.com>
2024-10-11 14:33:09 +0900

    20241011

--- Day01/day04/src/exam02/Student.java
+++ Day01/day04/src/exam02/Student.java
@@ -4,6 +4,11 @@ public class Student {
     int id;
     String name;
     String major;
+    /*
+    *  따로 정의하지 않으면 기본적으로 생산되는 생성자
+    * 자동으로 생성 된다
+    */
+    public Student(){}
     public void showInfo(){
         System.out.printf("id=%d, name=%s, major=%s%n",id,name,major);
     }
