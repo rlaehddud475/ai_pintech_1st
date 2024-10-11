10bd4815d6ef9d359c7dabe9621e10ea819323ed
rlaehddud475 <6070kjh475@naver.com>
2024-10-11 10:57:04 +0900

    20241011

--- Day01/day04/src/exam01/Ex01.java
+++ Day01/day04/src/exam01/Ex01.java
@@ -1,11 +1,22 @@
 package exam01;
 
 public class Ex01 {
+    /**
+     * main 함수는 프로그램을 시작해 주는 함수
+     * @param args
+     */
+
     public static void main(String[] args) {
         int r = add(10,20);
         System.out.println(r);
     }
-
+/*num1 num2 result -함수 지역 내에서만 유의미한 변수
+* -지역변수
+* @param num1
+* @param num2
+* @return
+*
+*/
     static int add(int num1, int num2){
         int result=num1+num2;
         return result;
