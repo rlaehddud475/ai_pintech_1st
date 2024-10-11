a17db39d6875652b056497d6f3474808b59310b6
rlaehddud475 <6070kjh475@naver.com>
2024-10-11 13:15:52 +0900

    20241011

--- Day01/day04/src/exam02/Ex01.java
+++ Day01/day04/src/exam02/Ex01.java
@@ -7,5 +7,23 @@ public class Ex01 {
         s1.name="이이름";
         s1.major="영문";
         s1.showInfo();
+
+        System.out.println("s1:"+System.identityHashCode(s1));
+    Student s2=new Student();
+    s2.id=1001;
+    s2.name="김이름";
+    s2.major="수학";
+    s2.showInfo();
+        System.out.println("s2:"+System.identityHashCode(s2));
+
+        Student s3=s2;
+s3.name="(수정)김이름";
+        s2.showInfo();
+        s3.showInfo();
+        System.out.println("s3:"+System.identityHashCode(s3));
+        System.out.println("s1,s2주소 : "+(s1==s2));
+        System.out.println("s2,s3주소 : "+(s2==s3));
+
+        System.out.println(s1);
     }
 }
