fc1dad1b633969aac2f93c0f97a187337490ecef
rlaehddud475 <6070kjh475@naver.com>
2024-10-14 10:48:59 +0900

    20241014

--- Day01/day05/src/exam01/Student.java
+++ Day01/day05/src/exam01/Student.java
@@ -5,7 +5,7 @@ public class Student {
     int id; //학번
     String name;//학생명
     String major;//전공
-
+public Student(){}
     public void showinfo(){
         System.out.printf("id=%d, name=%s, major=%s%n", id,name,major);
     }
---
+++ Day01/day05/src/exam03/A.java
@@ -0,0 +1,5 @@
+package exam03;
+
+public class A {
+    int num1;//default:같은 패키지에서만 접근가능
+}
