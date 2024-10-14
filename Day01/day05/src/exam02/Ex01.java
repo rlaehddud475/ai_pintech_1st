e7539a3a8f469f5b35836feabbb882ee304445f4
rlaehddud475 <6070kjh475@naver.com>
2024-10-14 11:52:53 +0900

    20241014

--- Day01/day05/src/exam02/Ex01.java
+++ Day01/day05/src/exam02/Ex01.java
@@ -3,9 +3,6 @@ package exam02;
 public class Ex01 {
     public static void main(String[] args) {
         Schedule sc1=new Schedule();
-        sc1.year=2024;
-        sc1.month=2;
-        sc1.day=31;//31이라는 잘못된 값을 검증하고 대입 통제하는 방법 없음
-        sc1.showDate();
+
     }
 }
--- Day01/day05/src/exam02/Schedule.java
+++ Day01/day05/src/exam02/Schedule.java
@@ -1,11 +1,34 @@
 package exam02;
 
 public class Schedule {
-    int year;
-    int month;
-    int day;
+    private int year;
+    private int month;
+    private int day;
     public void showDate() {
         System.out.printf("year=%d, month=%d day=%d%n",year,month,day);
     }
 
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
 }
