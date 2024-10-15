471cf7bf96d6ba740493c0be31d35a9d74c013f8
rlaehddud475 <6070kjh475@naver.com>
2024-10-15 17:11:24 +0900

    20241015

--- Day01/day06/src/exam03/Calculator.java
+++ Day01/day06/src/exam03/Calculator.java
@@ -1,5 +1,14 @@
 package exam03;
 
-public class Calculator {
-    
+import javax.sound.sampled.Line;
+
+public abstract class Calculator {
+  int num=10;
+
+  public Calculator() {
+    System.out.println("Calculator 생성자 호출");
+  }
+
+  public abstract int add(int num1, int num2);
+//    public abstract int sub(int num1, int num2);
 }
