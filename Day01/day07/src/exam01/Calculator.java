894c009fb390c93099f459d01c8591535a72d3f5
TwoNewbie <hgwon4071@gmail.com>
2024-10-16 09:59:21 +0900

    20241016

--- Day01/day07/src/exam01/Calculator.java
+++ Day01/day07/src/exam01/Calculator.java
@@ -1,5 +1,9 @@
 package exam01;
 
 public abstract class Calculator {
+    public Calculator() {
+        System.out.println("Calculator 생성자!");
+    }
+
     public abstract int add(int num1, int num2);
 }
