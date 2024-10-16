fd7021680d690bdcd59c67a5331858b56fba3dbe
TwoNewbie <hgwon4071@gmail.com>
2024-10-16 17:13:33 +0900

    20241016

--- Day01/day07/src/exam08/Outer.java
+++ Day01/day07/src/exam08/Outer.java
@@ -3,15 +3,21 @@ package exam08;
 import exam02.Calculator;
 
 public class Outer {
+    private static final Calculator cal=new Calculator() {
+        @Override
+        public int add(int num1, int num2) {
+            return num1+num2;
+        }
+    }
     //익명 내부 클래스
-    Calculator method(int num3){
-        return new Calculator() {
-            @Override
-            public int add(int num1, int num2) {
-                //num3=20; final num3 .. 지역 변수의 상수화, 제거 되지 않고 유지하기 위해서
-                return num1+num2+num3;
-            }
-        };
+//    Calculator method(int num3){
+//        return new Calculator() {
+//            @Override
+//            public int add(int num1, int num2) {
+//                //num3=20; final num3 .. 지역 변수의 상수화, 제거 되지 않고 유지하기 위해서
+//                return num1+num2+num3;
+//            }
+//        };
         /*Calculator cal = new Calculator(){
             @Override
             public int add(int num1, int num2) {
