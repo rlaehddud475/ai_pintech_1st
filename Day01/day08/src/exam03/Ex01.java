7e741f63f4627c739c3299f08f56297ea220a918
TwoNewbie <hgwon4071@gmail.com>
2024-10-17 15:00:03 +0900

    20241017

--- Day01/day08/src/exam03/Ex01.java
+++ Day01/day08/src/exam03/Ex01.java
@@ -1,7 +1,7 @@
 package exam03;
 import java.lang.*;
 
-public class Ex01 {
+public class Ex01 extends java.lang.Object{
     public static void main(String[] args) {
     String str = "안녕하세요";
     }
