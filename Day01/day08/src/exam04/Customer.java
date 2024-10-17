00ac74e5d879bfebef8f57a753b27d98b112a25c
TwoNewbie <hgwon4071@gmail.com>
2024-10-17 15:53:39 +0900

    20241017

--- Day01/day08/src/exam04/Customer.java
+++ Day01/day08/src/exam04/Customer.java
@@ -1,14 +1,23 @@
 package exam04;
 
-public class Customer {
+public class Customer extends java.lang.Object {
     private int id;
     private String name;
     private String email;
 
-    public Customer(String email, int id, String name) {
-        this.email = email;
+    public Customer(int id, String name, String email) {
         this.id = id;
         this.name = name;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "email='" + email + '\'' +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
     }
 
     public String getEmail() {
---
+++ Day01/day08/src/exam04/Ex02.java
@@ -0,0 +1,12 @@
+package exam04;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+        System.out.println(date);
+
+    }
+}
