10b70b2bec68c0771e878b091114c7444f6e6d1c
TwoNewbie <hgwon4071@gmail.com>
2024-10-24 18:21:57 +0900

    20241024

--- day12/src/exam02/Ex01.java
+++ day12/src/exam02/Ex01.java
@@ -1,7 +1,17 @@
 package exam02;
 
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.IntBinaryOperator;
+
 public class Ex01 {
     public static void main(String[] args) {
-
+//        BiFunction<Integer,Integer,Integer> cal=(a,b)->a+b;
+//        int result = cal.apply(10,30);
+//        System.out.println(result);
+//        BinaryOperator<Integer> cal = (a,b)->a+b;
+//        System.out.println((int)cal.apply(10,20));
+        IntBinaryOperator cal = (a,b)->a+b;
+        System.out.println(cal.applyAsInt(10,20  ));
     }
 }
