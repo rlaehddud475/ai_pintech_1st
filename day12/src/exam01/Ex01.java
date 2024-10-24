7f572662b46367c84dcbdccd81a93b84e38344c2
TwoNewbie <hgwon4071@gmail.com>
2024-10-24 16:12:30 +0900

    20241024

--- day12/src/exam01/Ex01.java
+++ day12/src/exam01/Ex01.java
@@ -2,17 +2,28 @@ package exam01;
 
 public class Ex01 {
     public static void main(String[] args) {
-        Calculator cal = new Calculator() {
-            @Override
-            public int add(int num1, int num2) {
+
+        int result = add2((num1, num2)-> {
                 return num1+num2;
-            }
+        },10,20);
+         /*
+           @Override
+           public int add(int num1, int num2) {
+               return num1+num2;
+           }
 
-        };
+       };
+       int result = add2(cal,10,20);
+       System.out.println(result);
+   }*/
+
+        System.out.println(result);
+
     }
-    public static int add2(Calculator cal, int num1, int num2){
+    public static int add2 (Calculator cal,int num1, int num2){
 
-        return cal.add(num1,num2);
+        return cal.add(num1, num2);
     }
 }
+
 
