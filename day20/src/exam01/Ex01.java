6813a1105fa49e20cc8aa1903d9c80b224677c7f
TwoNewbie <hgwon4071@gmail.com>
2024-11-07 16:12:31 +0900

    20241107

---
+++ day20/src/exam01/Ex01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+import java.util.Arrays;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int[] nums={20,10,5,99,30,21,45};
+        int total=Arrays.stream(nums).reduce(0, (acc, n)->{
+           return acc+n;
+        });
+        System.out.println(total);
+    }
+}
