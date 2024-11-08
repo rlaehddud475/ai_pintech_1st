dddc13809241e572e421ae0445f9eb1b3a224d37
TwoNewbie <hgwon4071@gmail.com>
2024-11-08 10:01:36 +0900

    20241108

--- day21/src/exam01/Ex01.java
+++ day21/src/exam01/Ex01.java
@@ -1,4 +1,21 @@
 package exam01;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Ex01 {
+    public static void main(String[] args) {
+try( FileInputStream fis=new FileInputStream("a.txt")) {
+int ch =fis.read();
+    System.out.println((char) ch);
+ch=fis.read();
+    System.out.println((char)ch);
+    ch=fis.read();
+    System.out.println((char)ch);
+    ch=fis.read();
+    System.out.println((char)ch);
+}catch (IOException e){
+    e.printStackTrace();
+}
+    }
 }
